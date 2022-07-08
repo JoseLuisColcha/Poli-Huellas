eaf97c630d398f5f0b98cae88d65e5b451cbc61d
JoseLuisColcha <jose.colcha@epn.edu.ec>
2022-07-07 22:42:04 -0500

    Agregar la página para dar en adopcion

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -6,7 +6,6 @@ const publicRoutes = {
   DOGS: "/perros",
   OTHER: "/otros",
   INSTRUCTIONS: "/instrucciones-de-adopcion",
-  GIVE_PET: "/dar-en-adopcion",
   FORGOT_PASSWORD: "/resetear-contrasenia",
 };
 
@@ -14,6 +13,7 @@ const privateRoutes = {
   HOME: "/",
   USERPROFILE: "/perfil-de-usuario",
   ADMIN: "/admin/perfil",
+  GIVE_PET: "/dar-en-adopcion",
 };
 
 const Routes = {
