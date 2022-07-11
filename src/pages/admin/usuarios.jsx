c455937dc46463bd5272503fe833a08917bd470f
JoseLuisColcha <jose.colcha@epn.edu.ec>
2022-07-10 22:09:17 -0500

    add list and view of users for admin

---
+++ src/pages/admin/usuarios.jsx
@@ -0,0 +1,50 @@
+import withAuth from "@/hocs/withAuth";
+import { useAuth } from "@/lib/auth";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import {Visibility, Delete} from '@mui/icons-material'
+import { useRouter } from "next/router";
+
+function Users() {
+  const { users } = useAuth();
+  const router = useRouter()
+  return (
+    <TableContainer component={Paper} sx={{ mt: '1.5rem' }}>
+      <Table sx={{ minWidth: 'auto' }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Usuario</TableCell>
+            <TableCell align="center">correo</TableCell>
+            <TableCell align="right">acciones</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users?.map((user) => (
+            <TableRow
+              key={user.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {user.name}
+              </TableCell>
+              <TableCell align="center">{user.email}</TableCell>
+              <TableCell sx={{display: 'flex', justifyContent: 'space-around'}}>
+                <Visibility color="primary" titleAccess="Ver usuario" onClick={() => router.push(`/perfil-de-usuario/${user.uid}`)}/>
+                <Delete color="error" titleAccess="Eliminar"/>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default withAuth(Users);
