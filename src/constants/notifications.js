850e700fdb775ae77c61327d586bfc1ede140bd8
JoseLuisColcha <jose.colcha@epn.edu.ec>
2022-08-30 02:42:25 -0500

    change notifications and user admin

--- src/constants/notifications.js
+++ src/constants/notifications.js
@@ -1,13 +1,14 @@
 const NOTIFICATIONS = {
-  ADOPTION: "quiere adoptar la mascota que publicaste. Revisa tus solicitudes de adopción",
+  ADOPTION:
+    "quiere adoptar la mascota que publicaste. Revisa tus solicitudes de adopción",
   REQUEST_ADOPTION:
     "Felicidades!! La mascota que solicitaste adoptar es tuya, el propietario se contactará contigo",
   BAD_REQUEST_ADOPTION:
     "Tu solicitud de adopción fue denegada, pero puedes seguir buscando otra mascota.",
   COMMENTED: "comento tu publicación.",
-  ACCEPTED_POST: "Tu publicación ha sido aceptada, puedes mirarla en tu perfil.",
+  ACCEPTED_POST:
+    "Tu publicación ha sido aceptada, puedes mirarla en tu perfil.",
   REJECTED_POST:
-    "Lo sentimos tu publicación ha sido rechazada, en unas horas será eliminada de tu perfil.",
+    "Lo sentimos tu publicación ha sido rechazada, en unas horas será eliminada completamente del sistema.",
 };
-
 export default NOTIFICATIONS;
