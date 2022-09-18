d9b370439dcdd1ca739d1cac966fc5d24504360c
JoseLuisColcha <jose.colcha@epn.edu.ec>
2022-09-18 16:04:20 -0500

    change favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,12 +5,31 @@ import createEmotionCache from "../../src/createEmotionCache";
 import theme from "@/styles/theme";
 
 export default class MyDocument extends Document {
-  render(){
+  render() {
     return (
       <Html>
         <Head>
-          <meta name="theme-color" 
-            content={theme.palette.primary.main} />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#2b5797" />
+          <meta name="theme-color" content="#ffffff"></meta>
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
